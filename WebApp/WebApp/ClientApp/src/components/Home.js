efb01e3952e38c9bde852c07852563bdf743a557
Gogo Dev <gogomagnusdev@gmail.com>
2021-01-26 08:52:27 +0100

    login gui not finished yet

--- WebApp/WebApp/ClientApp/src/components/Home.js
+++ WebApp/WebApp/ClientApp/src/components/Home.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 
 export class Home extends Component {
-  static displayName = Home.name;
+    static displayName = Home.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Welcome to FlightX</h1>
-        <div class="">
-
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to FlightX</h1>
+                <div class="">
+                    <h3>Login</h3>
+                    <form method="POST">
+                        <label htmlFor="username">Username</label>
+                        <input id="username" name="username" type="text" />
+                        <label htmlFor="password">Password</label>
+                        <input id="password" type="password" name="password" />
+                        <button type="submit">Login</button>
+                    </form>
+                </div>
+            </div>
+        );
+    }
 }
